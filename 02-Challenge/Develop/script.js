5cca749325c3c399dfd96d983de995c760112b69
themuppetman <sethjackson@sethshows.com>
2024-02-24 12:09:44 -0800

    password not generating

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
  
     const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
     const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -17,15 +17,23 @@ function generatePassword(length, includeLowercase, includeUppercase, includeNum
     allowedChars += includeLowercase ? lowercaseChars : "";
     allowedChars += includeUppercase ? uppercaseChars : "";
     allowedChars += includeNumbers ? numberChars : "";
-    allowedChars += includeSymbols ? symbolChars :"";
-
-    if(length <= 0){
-        return '(password length must be at least 1)';
-    }
+    allowedChars += includeSymbols ? symbolChars : "";
 
     console.log(allowedChars);
+                      
+    if(length <= 0){
+        return "(password length must be at least 1)";
+    }
+    if(allowedChars.length === 0){
+        return "(at least 1 set of characters needs to be selected)"
+    }
 
-    return '';
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
 }
 
 const passwordLength = 12;
@@ -35,12 +43,13 @@ const includeNumbers = true;
 const includeSymbols = true;
 
 const password = generatePassword(passwordLength,   
-                               includeLowercase, 
-                               includeUppercase,
-                               includeNumbers,
-                               includeSymbols);
+                                  includeLowercase, 
+                                  includeUppercase,
+                                  includeNumbers,
+                                  includeSymbols); 
 
 
+console.log("Generated password: {passowrd}");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+
